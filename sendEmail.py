2b03415c0878c41a6c9edff92f342fb33b741694
GabrielRibeirovoya <gabrielsribeiro3312@gmail.com>
2025-01-17 17:43:20 -0300

    Adjusting shipping libraries

--- sendEmail.py
+++ sendEmail.py
@@ -1,11 +1,10 @@
-import start 
+import RequestTime.requestTime as requestTime
 import os
 import smtplib
 import email.message
 import json
 import requests
 from dotenv import load_dotenv
-input('')
 load_dotenv()
 
 key = os.getenv('KEY_EMAIL')
@@ -25,7 +24,7 @@ def info():
 
     city = data['city']
     formatting = data['formatting']
-    data_time =  start.colectorTime(city, formatting)
+    data_time = requestTime.request_city(city, formatting)
     body = f'O tempo na cidade de {city} estará com {data_time['weather']} com a seguinte descrição: {data_time['description']}. A temperatura será em torno de {data_time['temp']}°C, com a mínima chegando a {data_time['temp_min']}°C e a máxima podendo atingir {data_time['temp_max']}°C. A umidade relativa do ar está em {data_time['humidity']}% e a pressão atmosférica é de {data_time['pressure']} hPa.'
 
     send_email('Clima do Dia', data['email'], body)
